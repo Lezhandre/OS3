e293eda1285721fed3b54d5653d6f34f64f2837f
Lezhandre <duzhenko.misha@mail.ru>
2022-03-26 00:46:57 +0300

    Bug-fixing 1) Return code handling 2) Backspace handling after UP and DOWN pressing [TO DO] history view + pipe cleaning

--- main.c
+++ main.c
@@ -16,10 +16,10 @@
 #define D_ARG 5     // изменение выделения памяти под указатели
 
 WINDOW * wnd;
-int child_id;
+int id;
 int bash_hist_desc;
 
-// функция для считывания строки без пробелов и переносов
+// функция для считывания строки без переноса
 // при этом перенос строки сохраняется
 int mygetstr(char * str, int * len){
     chtype c;
@@ -51,9 +51,8 @@ int mygetstr(char * str, int * len){
             return -1;
         case KEY_UP:
             {
-            char minibuf[2];
+            char minibuf[1];
             ssize_t read_num, in = 0;
-            //lseek(bash_hist_desc, , SEEK_CUR);
             while ((read_num = lseek(bash_hist_desc, -2, SEEK_CUR)) >= 0) {
                 read_num = read(bash_hist_desc, minibuf, 1);
                 if (minibuf[0] == '\n') break;
@@ -61,44 +60,42 @@ int mygetstr(char * str, int * len){
             }
             if (read_num < 0)
                 lseek(bash_hist_desc, 0, SEEK_SET);
-            if (in > 0) { //if smth was actually read
+            if (in > 0) { // если что-то прочиталось
                 if (wdeleteln(stdscr) == ERR) return -1;
-                str = (char *)realloc(str, in + 1);
+                refresh();
+                str = (char *)realloc(str, in + 2);
                 pread(bash_hist_desc, str, in, lseek(bash_hist_desc, 0, SEEK_CUR));
-                *len = in;
+                *len = in + 1;
                 str[in] = 0;
-                printw("\r%s", cwd);
+                printw("\r%s$ %s", cwd, str);
                 refresh();
-                printw("$ %s", str);
-                refresh();
-                i = *len;
+                i = in;
             }
-            else --i;
             }
+            --i;
             break;
         case KEY_DOWN:
             {
-            char minibuf[2];
+            char minibuf[1];
             size_t in = 0, read_num;
             if (read(bash_hist_desc, minibuf, 1) > 0 && minibuf[0] != '\n') {
                 ++in;
                 while (read(bash_hist_desc, minibuf, 1) > 0 && minibuf[0] != '\n')
                     ++in;
-                char cwd[PATH_MAX];
                 if (wdeleteln(stdscr) == ERR) return -1;
-                str = (char *)realloc(str, in + 1);
-                pread(bash_hist_desc, str, in, lseek(bash_hist_desc, -in - 1, SEEK_CUR));
-                lseek(bash_hist_desc, +in + 1, SEEK_CUR);
-                *len = in;
+                refresh();
+                str = (char *)realloc(str, in + 2);
+                pread(bash_hist_desc, str, in, lseek(bash_hist_desc, 0, SEEK_CUR) - in - 1);
+                *len = in + 1;
                 str[in] = 0;
                 printw("\r%s", cwd);
                 refresh();
                 printw("$ %s", str);
                 refresh();
-                i = *len;
+                i = in;
             }
-            else --i;
             }
+            --i;
             break;
         default:
             str[i] = c;
@@ -119,7 +116,7 @@ void check_mem(void * ptr){
 }
 
 void kill_child(int sig){
-    int err = kill(child_id, sig); // посылание какого-то сигнала в дочерний процесс
+    int err = kill(id, sig); // посылание какого-то сигнала в дочерний процесс
     if (err > 0)
         putp("Child process died\r");
     (void) signal(SIGINT, SIG_IGN);
@@ -127,7 +124,7 @@ void kill_child(int sig){
 
 int main(){
     wnd = initscr();
-    scrollok(wnd, true); // для ультрамегасупердуперпролистывания (чтобы вывод ещё больше не портился)
+    scrollok(wnd, true); // для того, чтобы врубить пролистывание (чтобы отрисовывалось нормально)
     init_pair(1, COLOR_BLACK, COLOR_GREEN); // скорее всего не работает (видимо цвет меняется только в настройках окна)
     keypad(wnd, true);  // нужно для включения особых клавиш (надо самостоятельно запрограммировать поведение)
     char cwd[PATH_MAX], minibuf[1], buf[BUFSIZ];
@@ -148,9 +145,7 @@ int main(){
         int len;
         char * cur_str;
         // цикл для получения с клавиатуры комманды/программы и её параметров
-        len = lseek(bash_hist_desc, -1, SEEK_END);
-        if (len == -1)
-            lseek(bash_hist_desc, 0, SEEK_END);
+        len = lseek(bash_hist_desc, 0, SEEK_END);
         do {
             cur_str = NULL;
             len = 1;
@@ -171,6 +166,7 @@ int main(){
         argv = (char**)malloc(D_ARG * sizeof(char *));
         check_mem((void *)argv);
         cur_arg_num = D_ARG;
+        len = strlen(cur_str);
         argv[i] = strtok(cur_str, delim);
         while (argv[i] != NULL) {
             ++i;
@@ -182,39 +178,11 @@ int main(){
             argv[i] = strtok(NULL, delim);
         }
         cur_arg_num = i;
-        lseek(bash_hist_desc, 0, SEEK_SET);
-        bool new = true, same = true;
-        while (read(bash_hist_desc, minibuf, 1) > 0) {
-            if (strcmp(minibuf, " ") == 0) {
-                if (argv[i] == NULL) continue;
-                if (strcmp(argv[i], buf) != 0) same = false;
-                else ++i;
-            } 
-            else if (strcmp(minibuf, "\n") == 0) {
-                if (same) {
-                    new = false;
-                    break;
-                }
-                buf[0] = 0;
-                i = 0;
-            }
-            else {
-                if (argv[i] == NULL) continue;
-                strcat(buf, minibuf);
-            }
-        }
-        if (new) {
-            lseek(bash_hist_desc, 0, SEEK_END);
-            for (size_t i = 0; argv[i]; ++i) {
-                write(bash_hist_desc, argv[i], strlen(argv[i]));
-                write(bash_hist_desc, " ", 1);
-            }
-            write(bash_hist_desc, "\n", 1);
-        }
         putp("\r");
         refresh();
         putp("\n");
         refresh();
+        if(!argv[0]) continue;
         if (strcmp(argv[0], "exit") == 0) {
             if (cur_arg_num > 1) {
                 printw("Too many arguments!");
@@ -235,12 +203,11 @@ int main(){
                 int err = chdir(argv[1]);
                 getcwd(cwd, PATH_MAX);
                 if (err == -1) {
-                    printw("Can't go to this directory: ");
-                    printw(strerror(errno));
+                    printw("Can't go to this directory: %s\n", strerror(errno));
                     refresh();
-                    y = getcury(wnd) + 1;
-                } else {
                     y = getcury(wnd);
+                } else {
+                    y = getcury(wnd) + 1;
                 }
             }
         } 
@@ -251,8 +218,8 @@ int main(){
                 refresh();
                 return 0;
             }
-            child_id = fork();
-            switch (child_id)
+            id = fork();
+            switch (id)
             {
             case -1:
                 printw("Critical error!!!\r");
@@ -262,30 +229,64 @@ int main(){
                 close(file_desc[0]);
                 dup2(file_desc[1], STDOUT_FILENO);
                 dup2(file_desc[1], STDERR_FILENO);
-                return execvp(*argv, argv);
+                close(file_desc[1]);
+                if (execvp(*argv, argv) == -1)
+                    exit(errno);
             default: // процесс-родитель
-                { // field of view
+                { // field of view (like pov)
                 close(file_desc[1]);
                 (void) signal(SIGINT, kill_child);
-                wait(NULL);
-                char buf[BUFSIZ];
-                int len;
-                while ((len = read(file_desc[0], buf, BUFSIZ)) > 0) {
-                    printw("%*s", len, buf);
-                    refresh();
+                int st;
+                waitpid(id, &st, 0);
+                if (WEXITSTATUS(st) == 0) {
+                    char buf[BUFSIZ];
+                    int lenin;
+                    while ((lenin = read(file_desc[0], buf, BUFSIZ)) > 0) {
+                        printw("%*s", lenin, buf);
+                        refresh();
+                    }
+                    if (lenin < 0) {
+                        printw("Reading problem!!!");
+                        refresh();
+                        return 0;
+                    }
+                    int tmp_y = getcury(wnd);
+                    if (tmp_y > y) {
+                        y = tmp_y;
+                    }
+                    // добавление в историю
+                    if (lseek(bash_hist_desc, -2, SEEK_END) < 0)
+                        lseek(bash_hist_desc, 0, SEEK_END);
+                    bool new = false;
+                    for (size_t i = 0; read(bash_hist_desc, minibuf, 1) > 0 && *minibuf != '\n'; ++i) {
+                        lseek(bash_hist_desc, -2, SEEK_CUR);
+                        if (len < 1 + i || *minibuf != cur_str[len - 1 - i] && !(!cur_str[len - 1 - i] && *minibuf == ' ')) {
+                            new = true;
+                            break;
+                        }
+                    }
+                    if (new) {
+                        lseek(bash_hist_desc, 0, SEEK_END);
+                        for (size_t i = 0; argv[i]; ++i) {
+                            write(bash_hist_desc, argv[i], strlen(argv[i]));
+                            if (i + 1 < cur_arg_num) write(bash_hist_desc, " ", 1);
+                        }
+                        write(bash_hist_desc, "\n", 1);
+                    }
                 }
-                if (len < 0) {
-                    printw("Reading problem!!!");
+                else {
+                    printw("%s", strerror(WEXITSTATUS(st)));
                     refresh();
-                    return 0;
+                    printw("\n");
+                    refresh();
+                    int tmp_y = getcury(wnd);
+                    if (tmp_y < y)
+                        ++y;
+                    else if (tmp_y > y) {
+                        y = tmp_y;
+                    }
                 }
-                int tmp_y = getcury(wnd);
-                if (tmp_y > y)
-                    y = tmp_y;
-                else if (tmp_y == y)
-                    ++y;
-                else
-                    printw("Could not find command!");
+                close(file_desc[0]);
                 }
             }
         }
